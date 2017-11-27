fa412493b63e9b93e6efa4b651d94ca4535fb0b5
Elizabeth Nanthavong <enanthav@gmail.com>
2017-11-26 17:15:05 -0700

    Updated alluvial graph for direct care area

--- js/graph.js
+++ js/graph.js
@@ -1,4 +1,10 @@
-$("#btn06AB").click(function(){
+
+
+/* GRAPH 06 ALLUVIAL GROUPS */
+
+
+
+$("#btn06taliban").click(function(){
     $(".G06grouprect").removeClass( "G06recton" );
     $("#G06grouprecttaliban").addClass( "G06recton" );
     $(".G06gruppi").removeClass( "idle" );
@@ -8,7 +14,7 @@ $("#btn06AB").click(function(){
 
 });
 
-$("#btn06BC").click(function(){
+$("#btn06isis").click(function(){
     $(".G06grouprect").removeClass( "G06recton" );
     $("#G06grouprectisis").addClass( "G06recton" );
     $(".G06gruppi").removeClass( "idle" );
@@ -96,4 +102,131 @@ $("#btn06qaida").click(function(){
     $('.G06flussi').removeClass( "G06on" );
     $('#G06flussoqaida').addClass( "G06on" );
 
+});
+
+
+
+
+
+
+
+
+
+
+$("#btn06AB").click(function(){
+    $(".G06grouprect").removeClass( "G06recton" );
+    $("#G06grouprectAB").addClass( "G06recton" );
+    $(".G06gruppi").removeClass( "idle" );
+    $(this).addClass( "idle" );
+    $('.G06flussi').removeClass( "G06on" );
+    $('#G06nodeAB').addClass( "G06on" );
+});
+
+$("#btn06BC").click(function(){
+    $(".G06grouprect").removeClass( "G06recton" );
+    $("#G06grouprectBC").addClass( "G06recton" );
+    $(".G06gruppi").removeClass( "idle" );
+    $(this).addClass( "idle" );
+    $('.G06flussi').removeClass( "G06on" );
+    $('#G06nodeBC').addClass( "G06on" );
+});
+
+$("#btn06MB").click(function(){
+    $(".G06grouprect").removeClass( "G06recton" );
+    $("#G06grouprectMB").addClass( "G06recton" );
+    $(".G06gruppi").removeClass( "idle" );
+    $(this).addClass( "idle" );
+    $('.G06flussi').removeClass( "G06on" );
+    $('#G06nodeMB').addClass( "G06on" );
+});
+
+$("#btn06NB").click(function(){
+    $(".G06grouprect").removeClass( "G06recton" );
+    $("#G06grouprectNB").addClass( "G06recton" );
+    $(".G06gruppi").removeClass( "idle" );
+    $(this).addClass( "idle" );
+    $('.G06flussi').removeClass( "G06on" );
+    $('#G06nodeNB').addClass( "G06on" );
+});
+
+$("#btn06NL").click(function(){
+    $(".G06grouprect").removeClass( "G06recton" );
+    $("#G06grouprectNL").addClass( "G06recton" );
+    $(".G06gruppi").removeClass( "idle" );
+    $(this).addClass( "idle" );
+    $('.G06flussi').removeClass( "G06on" );
+    $('#G06nodeNL').addClass( "G06on" );
+});
+
+$("#btn06NT").click(function(){
+    $(".G06grouprect").removeClass( "G06recton" );
+    $("#G06grouprectNT").addClass( "G06recton" );
+    $(".G06gruppi").removeClass( "idle" );
+    $(this).addClass( "idle" );
+    $('.G06flussi').removeClass( "G06on" );
+    $('#G06nodeNT').addClass( "G06on" );
+
+});
+
+$("#btn06NS").click(function(){
+    $(".G06grouprect").removeClass( "G06recton" );
+    $("#G06grouprectNS").addClass( "G06recton" );
+    $(".G06gruppi").removeClass( "idle" );
+    $(this).addClass( "idle" );
+    $('.G06flussi').removeClass( "G06on" );
+    $('#G06nodeNS').addClass( "G06on" );
+});
+
+$("#btn06BC").click(function(){
+    $(".G06grouprect").removeClass( "G06recton" );
+    $("#G06grouprectBC").addClass( "G06recton" );
+    $(".G06gruppi").removeClass( "idle" );
+    $(this).addClass( "idle" );
+    $('.G06flussi').removeClass( "G06on" );
+    $('#G06nodeBC').addClass( "G06on" );
+});
+
+$("#btn06ON").click(function(){
+    $(".G06grouprect").removeClass( "G06recton" );
+    $("#G06grouprectON").addClass( "G06recton" );
+    $(".G06gruppi").removeClass( "idle" );
+    $(this).addClass( "idle" );
+    $('.G06flussi').removeClass( "G06on" );
+    $('#G06nodeON').addClass( "G06on" );
+});
+
+$("#btn06PE").click(function(){
+    $(".G06grouprect").removeClass( "G06recton" );
+    $("#G06grouprectPE").addClass( "G06recton" );
+    $(".G06gruppi").removeClass( "idle" );
+    $(this).addClass( "idle" );
+    $('.G06flussi').removeClass( "G06on" );
+    $('#G06nodePE').addClass( "G06on" );
+});
+
+$("#btn06QC").click(function(){
+    $(".G06grouprect").removeClass( "G06recton" );
+    $("#G06grouprectQC").addClass( "G06recton" );
+    $(".G06gruppi").removeClass( "idle" );
+    $(this).addClass( "idle" );
+    $('.G06flussi').removeClass( "G06on" );
+    $('#G06nodeQC').addClass( "G06on" );
+});
+
+$("#btn06SK").click(function(){
+    $(".G06grouprect").removeClass( "G06recton" );
+    $("#G06grouprectSK").addClass( "G06recton" );
+    $(".G06gruppi").removeClass( "idle" );
+    $(this).addClass( "idle" );
+    $('.G06flussi').removeClass( "G06on" );
+    $('#G06nodeSK').addClass( "G06on" );
+});
+
+$("#btn06YT").click(function(){
+    $(".G06grouprect").removeClass( "G06recton" );
+    $("#G06grouprectYT").addClass( "G06recton" );
+    $(".G06gruppi").removeClass( "idle" );
+    $(this).addClass( "idle" );
+    $('.G06flussi').removeClass( "G06on" );
+    $('#G06nodeYT').addClass( "G06on" );
 });
