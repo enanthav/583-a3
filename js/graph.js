ae52c163e39c439b075a2e6dab20405a74745a75
Elizabeth <enanthav@gmail.com>
2017-11-30 21:29:42 -0700

    Fixed formatting of main page

--- js/graph.js
+++ js/graph.js
@@ -1,54 +1,54 @@
 $("#btn06AB").click(function(){
     $(".G06grouprect").removeClass( "G06recton" );
     $("#G06grouprectAB").addClass( "G06recton" );
-    $(".G06gruppi").removeClass( "idle" );
+    $(".G06alluvial").removeClass( "idle" );
     $(this).addClass( "idle" );
-    $('.G06flussi').removeClass( "G06on" );
+    $('.G06flow').removeClass( "G06on" );
     $('#G06nodeAB').addClass( "G06on" );
 });
 
 $("#btn06BC").click(function(){
     $(".G06grouprect").removeClass( "G06recton" );
     $("#G06grouprectBC").addClass( "G06recton" );
-    $(".G06gruppi").removeClass( "idle" );
+    $(".G06alluvial").removeClass( "idle" );
     $(this).addClass( "idle" );
-    $('.G06flussi').removeClass( "G06on" );
+    $('.G06flow').removeClass( "G06on" );
     $('#G06nodeBC').addClass( "G06on" );
 });
 
 $("#btn06MB").click(function(){
     $(".G06grouprect").removeClass( "G06recton" );
     $("#G06grouprectMB").addClass( "G06recton" );
-    $(".G06gruppi").removeClass( "idle" );
+    $(".G06alluvial").removeClass( "idle" );
     $(this).addClass( "idle" );
-    $('.G06flussi').removeClass( "G06on" );
+    $('.G06flow').removeClass( "G06on" );
     $('#G06nodeMB').addClass( "G06on" );
 });
 
 $("#btn06NB").click(function(){
     $(".G06grouprect").removeClass( "G06recton" );
     $("#G06grouprectNB").addClass( "G06recton" );
-    $(".G06gruppi").removeClass( "idle" );
+    $(".G06alluvial").removeClass( "idle" );
     $(this).addClass( "idle" );
-    $('.G06flussi').removeClass( "G06on" );
+    $('.G06flow').removeClass( "G06on" );
     $('#G06nodeNB').addClass( "G06on" );
 });
 
 $("#btn06NL").click(function(){
     $(".G06grouprect").removeClass( "G06recton" );
     $("#G06grouprectNL").addClass( "G06recton" );
-    $(".G06gruppi").removeClass( "idle" );
+    $(".G06alluvial").removeClass( "idle" );
     $(this).addClass( "idle" );
-    $('.G06flussi').removeClass( "G06on" );
+    $('.G06flow').removeClass( "G06on" );
     $('#G06nodeNL').addClass( "G06on" );
 });
 
 $("#btn06NT").click(function(){
     $(".G06grouprect").removeClass( "G06recton" );
     $("#G06grouprectNT").addClass( "G06recton" );
-    $(".G06gruppi").removeClass( "idle" );
+    $(".G06alluvial").removeClass( "idle" );
     $(this).addClass( "idle" );
-    $('.G06flussi').removeClass( "G06on" );
+    $('.G06flow').removeClass( "G06on" );
     $('#G06nodeNT').addClass( "G06on" );
 
 });
@@ -56,62 +56,62 @@ $("#btn06NT").click(function(){
 $("#btn06NS").click(function(){
     $(".G06grouprect").removeClass( "G06recton" );
     $("#G06grouprectNS").addClass( "G06recton" );
-    $(".G06gruppi").removeClass( "idle" );
+    $(".G06alluvial").removeClass( "idle" );
     $(this).addClass( "idle" );
-    $('.G06flussi').removeClass( "G06on" );
+    $('.G06flow').removeClass( "G06on" );
     $('#G06nodeNS').addClass( "G06on" );
 });
 
 $("#btn06BC").click(function(){
     $(".G06grouprect").removeClass( "G06recton" );
     $("#G06grouprectBC").addClass( "G06recton" );
-    $(".G06gruppi").removeClass( "idle" );
+    $(".G06alluvial").removeClass( "idle" );
     $(this).addClass( "idle" );
-    $('.G06flussi').removeClass( "G06on" );
+    $('.G06flow').removeClass( "G06on" );
     $('#G06nodeBC').addClass( "G06on" );
 });
 
 $("#btn06ON").click(function(){
     $(".G06grouprect").removeClass( "G06recton" );
     $("#G06grouprectON").addClass( "G06recton" );
-    $(".G06gruppi").removeClass( "idle" );
+    $(".G06alluvial").removeClass( "idle" );
     $(this).addClass( "idle" );
-    $('.G06flussi').removeClass( "G06on" );
+    $('.G06flow').removeClass( "G06on" );
     $('#G06nodeON').addClass( "G06on" );
 });
 
 $("#btn06PE").click(function(){
     $(".G06grouprect").removeClass( "G06recton" );
     $("#G06grouprectPE").addClass( "G06recton" );
-    $(".G06gruppi").removeClass( "idle" );
+    $(".G06alluvial").removeClass( "idle" );
     $(this).addClass( "idle" );
-    $('.G06flussi').removeClass( "G06on" );
+    $('.G06flow').removeClass( "G06on" );
     $('#G06nodePE').addClass( "G06on" );
 });
 
 $("#btn06QC").click(function(){
     $(".G06grouprect").removeClass( "G06recton" );
     $("#G06grouprectQC").addClass( "G06recton" );
-    $(".G06gruppi").removeClass( "idle" );
+    $(".G06alluvial").removeClass( "idle" );
     $(this).addClass( "idle" );
-    $('.G06flussi').removeClass( "G06on" );
+    $('.G06flow').removeClass( "G06on" );
     $('#G06nodeQC').addClass( "G06on" );
 });
 
 $("#btn06SK").click(function(){
     $(".G06grouprect").removeClass( "G06recton" );
     $("#G06grouprectSK").addClass( "G06recton" );
-    $(".G06gruppi").removeClass( "idle" );
+    $(".G06alluvial").removeClass( "idle" );
     $(this).addClass( "idle" );
-    $('.G06flussi').removeClass( "G06on" );
+    $('.G06flow').removeClass( "G06on" );
     $('#G06nodeSK').addClass( "G06on" );
 });
 
 $("#btn06YT").click(function(){
     $(".G06grouprect").removeClass( "G06recton" );
     $("#G06grouprectYT").addClass( "G06recton" );
-    $(".G06gruppi").removeClass( "idle" );
+    $(".G06alluvial").removeClass( "idle" );
     $(this).addClass( "idle" );
-    $('.G06flussi').removeClass( "G06on" );
+    $('.G06flow').removeClass( "G06on" );
     $('#G06nodeYT').addClass( "G06on" );
 });
